e9dd7a1100a6b036c2530ccbaf0fcc7a4fd97065
Melissa Holt <melissa.holt21@gmail.com>
2019-11-30 03:15:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,7 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/[aeiou]/)
+  false
+  word.scan(/[aeiou]/)
     true
-  else
-    false
-  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
