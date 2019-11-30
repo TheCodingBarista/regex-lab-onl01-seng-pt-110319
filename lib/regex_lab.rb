88e632dc4c6bd590e6bef3dfb9685c414d2e06e2
Melissa Holt <melissa.holt21@gmail.com>
2019-11-30 02:47:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.start_with?(\/[aeiou]/)
-    true
-  else
-    false
-  end
+  word.start_with?(\/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
